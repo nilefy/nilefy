02211ee709b9b566449b168310ddc12f7d13a6fe
nagy nabil <nagynabil651@gmail.com>
2023-10-15 20:09:03 +0300

    chore: init nest app with fastify/SWC

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
