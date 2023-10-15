008a87367fcb9ba216151f90023528f12e8bc86e
nagy nabil <nagynabil651@gmail.com>
2023-10-15 16:58:32 +0300

    chore[ui]: add shadcn seprator/card

--- apps/frontend/src/components/selectWorkspace.tsx
+++ apps/frontend/src/components/selectWorkspace.tsx
@@ -28,16 +28,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-
-/**
- * depends that the length of `str` is atleast 1 and split by spaces
- *
- * TODO: handle other spliters than space
- */
-function getInitials(str: string) {
-  const splits = str.split(' ', 2);
-  return `${splits[0][0]}${splits[1][0] ?? ''}`;
-}
+import { getInitials } from '@/utils/avatar';
 
 export type SelectWorkSpaceProps = {
   /**
