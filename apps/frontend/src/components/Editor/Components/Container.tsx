4d17769ab94e10cb2dbb8796817549bcf27430c9
zam <ahmed195138@feng.bu.edu.eg>
2023-10-17 21:03:06 +0300

    fix/container size not set to 100%

--- apps/frontend/src/components/Editor/Components/Container.tsx
+++ apps/frontend/src/components/Editor/Components/Container.tsx
@@ -5,7 +5,14 @@ const Container = forwardRef<
   { children: ReactNode; className: string }
 >(({ children, className }, ref) => {
   return (
-    <div ref={ref} className={className}>
+    <div
+      ref={ref}
+      className={className}
+      style={{
+        width: '100%',
+        height: '100%'
+      }}
+    >
       {children}
     </div>
   );
