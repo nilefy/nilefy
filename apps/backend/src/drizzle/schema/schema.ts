dd0b249699b859d03a0b145f647a44c7b965ba6b
nagy nabil <nagynabil651@gmail.com>
2023-10-30 23:21:54 +0200

    chore(db): update db schema - add workspace/app relation - add created by to workspaces/apps - add deleted by to workspaces/apps - add updated by to workspaces/apps

--- apps/backend/src/drizzle/schema/schema.ts
+++ apps/backend/src/drizzle/schema/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from 'drizzle-orm';
+import { relations, sql } from 'drizzle-orm';
 import {
   integer,
   pgTable,
@@ -9,7 +9,7 @@ import {
 } from 'drizzle-orm/pg-core';
 
 /**
- * spreading them to easy create createdAt and updatedAt fields
+ * spread them to easy create createdAt and updatedAt fields
  */
 export const timeStamps = {
   createdAt: timestamp('created_at')
@@ -33,19 +33,110 @@ export const users = pgTable('users', {
 
 export const workspaces = pgTable('workspaces', {
   id: serial('id').primaryKey(),
+  /**
+   * implicitly means the admin
+   */
+  createdById: integer('created_by_id')
+    .references(() => users.id)
+    .notNull(),
   name: varchar('name', { length: 256 }).notNull(),
   imageUrl: text('imageUrl'),
   ...timeStamps,
   ...softDelete,
+  updatedById: integer('updated_by_id').references(() => users.id),
+  deletedById: integer('deleted_by_id').references(() => users.id),
 });
 
 export const apps = pgTable('apps', {
   id: serial('id').primaryKey(),
-  name: varchar('name', { length: 100 }).unique().notNull(),
-  icon: text('icon'),
-  userId: integer('user_id')
+  createdById: integer('created_by_id')
     .references(() => users.id)
+    .notNull(),
+  name: varchar('name', { length: 100 }).notNull(),
+  description: varchar('description', { length: 255 }),
+  /**
+   * workspace this app belongs to
+   */
+  workspaceId: integer('workspace_id')
+    .references(() => workspaces.id)
     .notNull(),
   ...timeStamps,
   ...softDelete,
+  updatedById: integer('updated_by_id').references(() => users.id),
+  deletedById: integer('deleted_by_id').references(() => users.id),
 });
+
+const userWorkspaceRelation = 'userWorkspaces';
+const userAppRelation = 'userApps';
+const workspaceAppsRelation = 'workspaceApps';
+const userUpdateWorkspaceRelation = 'lastUpdatedWorkspaces';
+const userDeleteWorkspaceRelation = 'lastDeletedWorkspaces';
+const userUpdateAppRelation = 'lastUpdatedApps';
+const userDeleteAppRelation = 'lastDeletedApps';
+
+export const usersRelations = relations(users, ({ many }) => {
+  return {
+    lastUpdatedWorkspaces: many(workspaces, {
+      relationName: userUpdateWorkspaceRelation,
+    }),
+    lastDeletedWorkspaces: many(workspaces, {
+      relationName: userDeleteWorkspaceRelation,
+    }),
+    lastUpdatedApps: many(apps, {
+      relationName: userUpdateAppRelation,
+    }),
+    lastDeletedApps: many(apps, {
+      relationName: userDeleteAppRelation,
+    }),
+    /**
+     * workspaces user created/own
+     */
+    workspaces: many(workspaces, { relationName: userWorkspaceRelation }),
+    /**
+     * Apps user created/own
+     */
+    apps: many(apps, { relationName: userAppRelation }),
+  };
+});
+
+export const workspacesRelations = relations(workspaces, ({ many, one }) => ({
+  createdBy: one(users, {
+    fields: [workspaces.createdById],
+    references: [users.id],
+    relationName: userWorkspaceRelation,
+  }),
+  updatedBy: one(users, {
+    fields: [workspaces.updatedById],
+    references: [users.id],
+    relationName: userUpdateWorkspaceRelation,
+  }),
+  deletedBy: one(users, {
+    fields: [workspaces.deletedById],
+    references: [users.id],
+    relationName: userDeleteWorkspaceRelation,
+  }),
+  apps: many(apps, { relationName: workspaceAppsRelation }),
+}));
+
+export const appsRelations = relations(apps, ({ one }) => ({
+  createdBy: one(users, {
+    fields: [apps.createdById],
+    references: [users.id],
+    relationName: userAppRelation,
+  }),
+  updatedBy: one(users, {
+    fields: [apps.updatedById],
+    references: [users.id],
+    relationName: userUpdateAppRelation,
+  }),
+  deletedBy: one(users, {
+    fields: [apps.deletedById],
+    references: [users.id],
+    relationName: userDeleteAppRelation,
+  }),
+  workspace: one(workspaces, {
+    fields: [apps.workspaceId],
+    references: [workspaces.id],
+    relationName: workspaceAppsRelation,
+  }),
+}));
