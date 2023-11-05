d4310158d56871126842339e7f743ad008d65e2b
mhosnyessa <mhosny67710@gmail.com>
2023-11-06 00:21:32 +0200

    feat(backend): Add user faker, -Stuck at finding a way to add them through from outside nest as a script- working on the seed.db.ts script

---
+++ apps/backend/src/scripts/faker/user.faker.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker';
+
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
+
+export function generateFakeUser(): User {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    deletedAt: faker.helpers.arrayElement([null, null, faker.date.past()]),
+  };
+}
+
+export const user = generateFakeUser();
