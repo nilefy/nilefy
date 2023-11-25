d7f66d51968e1d44a56ac1304a62a3951378ba41
zam <ahmed195138@feng.bu.edu.eg>
2023-11-25 11:42:51 +0200

    added basic text widget

--- apps/frontend/src/pages/Editor/Components/index.ts
+++ apps/frontend/src/pages/Editor/Components/index.ts
@@ -1,11 +1,13 @@
 import { WebloomButtonWidget } from './WebloomWidgets/Button';
 import { WebloomContainerWidget } from './WebloomWidgets/Container';
 import { WebloomInputWidget } from './WebloomWidgets/Input';
+import { WebloomTextWidget } from './WebloomWidgets/Text';
 
 export const WebloomWidgets = {
   WebloomButton: WebloomButtonWidget,
   WebloomContainer: WebloomContainerWidget,
   WebloomInput: WebloomInputWidget,
+  WebloomText: WebloomTextWidget,
 } as const;
 
 export type WidgetTypes = keyof typeof WebloomWidgets;
