bf9c561ca96a4aafe095ce5da111e595bd703ac7
nagy nabil <nagynabil651@gmail.com>
2023-11-26 01:29:49 +0200

    chore(frontend): add debounced input

---
+++ apps/frontend/src/components/debouncedInput.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Input } from '@/components/ui/input';
+
+// A debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number;
+  onChange: (value: string | number) => void;
+  debounce?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
+  return (
+    <Input
+      {...props}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+}
+
+export default DebouncedInput;
