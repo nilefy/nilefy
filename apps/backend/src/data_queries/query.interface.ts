4619d2b64bd2841b674568b1a1bc1411d1707c44
nagy nabil <nagynabil651@gmail.com>
2023-11-28 21:38:32 +0200

    chore(backend): make QueryInterface genaric

--- apps/backend/src/data_queries/query.interface.ts
+++ apps/backend/src/data_queries/query.interface.ts
@@ -1,12 +1,17 @@
-import { AddQueryDto } from '../dto/data_queries.dto';
 import { DataSourceConfigT } from '../dto/data_sources.dto';
 import { QueryRet } from './query.types';
 
+type QueryConfig<T = unknown> = {
+  nama: string;
+  query: Record<string, T>;
+};
 export interface QueryRunnerI {
   run(
     dataSourceConfig: DataSourceConfigT,
-    query: AddQueryDto,
+    query: QueryConfig,
   ): Promise<QueryRet>;
 
   connect(dataSourceConfig: DataSourceConfigT): any;
+
+  testConnection?: (dataSourceConfig: DataSourceConfigT) => Promise<boolean>;
 }
