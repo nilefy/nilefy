17240a5e787edf59c13165fb228901548ad63372
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2023-12-04 18:21:22 +0200

    global data sources controller

---
+++ apps/backend/src/data_sources/global_data_sources.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { DatabaseI, DrizzleAsyncProvider } from '../drizzle/drizzle.provider';
+import {
+  DataSourceDto,
+  DataSourceDb,
+  DataSourceP,
+} from '../dto/data_sources.dto';
+import { dataSources } from '../drizzle/schema/data_sources.schema';
+import { eq } from 'drizzle-orm';
+
+@Injectable()
+export class GlobalDataSourcesService {
+  constructor(@Inject(DrizzleAsyncProvider) private db: DatabaseI) {}
+
+  async add(dataSource: DataSourceDb): Promise<DataSourceDto> {
+    const [ds] = await this.db
+      .insert(dataSources)
+      .values(dataSource)
+      .returning();
+    return ds as DataSourceDto;
+  }
+
+  async getAll(): Promise<DataSourceP[]> {
+    const ds = await this.db.query.dataSources.findMany({
+      columns: {
+        config: false,
+      },
+    });
+    return ds;
+  }
+
+  async getOne(dataSourceId: DataSourceDto['id']): Promise<DataSourceDto> {
+    const ds = await this.db.query.dataSources.findFirst({
+      where: eq(dataSources.id, dataSourceId),
+    });
+    return ds as DataSourceDto;
+  }
+}
