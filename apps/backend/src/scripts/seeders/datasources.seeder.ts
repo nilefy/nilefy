f499ba00b40247b282549218b204a18a33953768
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2023-12-09 21:47:52 +0200

    feat(data source): data sources seeder

--- apps/backend/src/scripts/seeders/datasources.seeder.ts
+++ apps/backend/src/scripts/seeders/datasources.seeder.ts
@@ -1,118 +1,160 @@
 import { faker } from '@faker-js/faker';
+import {
+  dataSources,
+  dataSourcesEnum,
+  DataSourceT,
+  ConfigT,
+} from '../seeders/seeder.types';
 import { DatabaseI } from '../../drizzle/drizzle.provider';
-import { DataSourceDto } from '../../dto/data_sources.dto';
-import { dataSources } from '../../drizzle/schema/data_sources.schema';
+import * as schema from '../../drizzle/schema/data_sources.schema';
+import {
+  PostgresqlConfigT,
+  PostgresqlQueryConfigT,
+} from '@webloom/data-sources';
 
-function generateFakeDataSource(): Omit<DataSourceDto, 'id'> {
-  return {
-    config: [
-      {
-        sectionName: 'Development',
-        children: [
-          {
-            id: 'host',
-            key: 'host',
-            label: 'Host',
-            type: 'input',
-            options: {
-              placeholder: 'localhost',
-              type: 'text',
-            },
-          },
-          {
-            id: 'port',
-            key: 'port',
-            label: 'Port',
-            type: 'input',
-            options: {
-              placeholder: 5000,
-              type: 'number',
-            },
-          },
-          {
-            id: 'ssl',
-            key: 'ssl',
-            label: 'SSL',
-            type: 'input',
-            options: {},
-          },
-          {
-            id: 'database_name',
-            key: 'database',
-            label: 'Database Name',
-            type: 'input',
-            options: {
-              placeholder: 'Name of the database',
-              type: 'text',
-            },
-          },
-          {
-            id: 'username',
-            key: 'user',
-            label: 'Username',
-            type: 'input',
-            options: {
-              placeholder: 'Enter username',
-              type: 'text',
-            },
-          },
-          {
-            id: 'password',
-            key: 'password',
-            label: 'Password',
-            type: 'input',
-            options: {
-              placeholder: 'Enter password',
-              type: 'password',
-            },
-          },
-          {
-            id: 'certificate',
-            key: 'sslCertificate',
-            label: 'SSL Certificate',
-            type: 'select',
-            options: {
-              items: [
-                {
-                  label: 'CA Certificate',
-                  value: 'ca',
-                },
-                {
-                  label: 'Self-signed Certificate',
-                  value: 'self-signed',
-                },
-                {
-                  label: 'None',
-                  value: 'none',
-                },
-              ],
-              placeholder: 'None',
-            },
-          },
-        ],
-      },
-    ],
-    image: faker.helpers.arrayElement([faker.image.url(), null]),
-    type: faker.helpers.arrayElement([
-      'database',
-      'api',
-      'cloud storage',
-      'plugin',
-    ]),
-    name: faker.commerce.productName(),
-    description: faker.helpers.arrayElement([
-      faker.commerce.productDescription(),
-      null,
-    ]),
-  };
-}
+export async function dataSourcesSeeder(db: DatabaseI) {
+  console.log('running DATA SOURCES seeder');
 
-export async function datasourcesSeeder(db: DatabaseI) {
-  console.log('running DATASOURCES seeder');
+  const ds: DataSourceT[] = [];
+  dataSourcesEnum.options.forEach((type) => {
+    dataSources[type].forEach((name) => {
+      let dataSourceConfig: unknown;
+      let queryConfig: unknown;
 
-  const fakeDS = faker.helpers.multiple(generateFakeDataSource, {
-    count: 50,
+      if (name === 'postgresql') {
+        const postgresqlConfig: ConfigT<PostgresqlConfigT> = [
+          {
+            sectionName: 'Development',
+            children: [
+              {
+                id: 'host',
+                key: 'host',
+                label: 'Host',
+                type: 'input',
+                options: {
+                  placeholder: 'localhost',
+                  type: 'text',
+                },
+              },
+              {
+                id: 'port',
+                key: 'port',
+                label: 'Port',
+                type: 'input',
+                options: {
+                  placeholder: 5000,
+                  type: 'number',
+                },
+              },
+              {
+                id: 'ssl',
+                key: 'ssl',
+                label: 'SSL',
+                type: 'input',
+                options: {},
+              },
+              {
+                id: 'database_name',
+                key: 'database',
+                label: 'Database Name',
+                type: 'input',
+                options: {
+                  placeholder: 'Name of the database',
+                  type: 'text',
+                },
+              },
+              {
+                id: 'username',
+                key: 'user',
+                label: 'Username',
+                type: 'input',
+                options: {
+                  placeholder: 'Enter username',
+                  type: 'text',
+                },
+              },
+              {
+                id: 'password',
+                key: 'password',
+                label: 'Password',
+                type: 'input',
+                options: {
+                  placeholder: 'Enter password',
+                  type: 'password',
+                },
+              },
+              {
+                id: 'certificate',
+                key: 'sslCertificate',
+                label: 'SSL Certificate',
+                type: 'select',
+                options: {
+                  items: [
+                    {
+                      label: 'CA Certificate',
+                      value: 'ca',
+                    },
+                    {
+                      label: 'Self-signed Certificate',
+                      value: 'self-signed',
+                    },
+                    {
+                      label: 'None',
+                      value: 'none',
+                    },
+                  ],
+                  placeholder: 'None',
+                },
+              },
+              // TODO: add connection options key-value pairs
+            ],
+          },
+        ];
+        const postgresqlQueryConfig: ConfigT<PostgresqlQueryConfigT> = [
+          {
+            sectionName: '',
+            children: [
+              {
+                id: 'connection',
+                key: 'dataSourceId',
+                label: 'Data Source',
+                type: 'select',
+                options: {
+                  items: [
+                    // fetch all data source connections
+                  ],
+                },
+              },
+              {
+                id: 'sql',
+                key: 'sql',
+                label: 'SQL',
+                type: 'input',
+                options: {},
+              },
+            ],
+          },
+        ];
+        dataSourceConfig = postgresqlConfig;
+        queryConfig = postgresqlQueryConfig;
+      } else {
+        dataSourceConfig = [];
+        queryConfig = [];
+      }
+
+      ds.push({
+        type,
+        name,
+        description: faker.helpers.arrayElement([
+          faker.commerce.productDescription(),
+          null,
+        ]),
+        image: faker.helpers.arrayElement([faker.image.url(), null]),
+        config: JSON.stringify(dataSourceConfig),
+        queryConfig: JSON.stringify(queryConfig),
+      });
+    });
   });
 
-  return await db.insert(dataSources).values(fakeDS).returning();
+  await db.insert(schema.dataSources).values(ds);
 }
