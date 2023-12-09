8bd0aebd35dcf1fb7dc955b20af8fd33b4d030da
Mohamedhadhoud1 <muhammed195837@feng.bu.edu.eg>
2023-12-09 16:54:19 +0200

    Data Sources api Done

--- apps/frontend/src/api/datasources.ts
+++ apps/frontend/src/api/datasources.ts
@@ -28,23 +28,87 @@ export type globalDataSource = {
   type: string;
 };
 
-export async function getDataSources({
-  workspaceId,
-}: {
-  workspaceId: string | undefined;
-}) {
+export async function getGlobalDataSources() {
+  const res = await fetchX(`data-sources/global`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return (await res.json()) as globalDataSource[];
+}
+
+export async function getGlobalDataSource(id: number) {
+  const res = await fetchX(`data-sources/global/${id}`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return (await res.json()) as globalDataSource;
+}
+export async function addGlobalDataSource(data: globalDataSource) {
+  const res = await fetchX(`data-sources/global`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  return (await res.json()) as globalDataSource;
+}
+
+export async function getDataSources({ workspaceId }: { workspaceId: number }) {
   const res = await fetchX(`workspaces/${workspaceId}/data-sources`, {
     method: 'GET',
   });
   return (await res.json()) as globalDataSource;
 }
 
-export async function getGlobalDataSources() {
-  const res = await fetchX(`data-sources/global`, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  });
-  return (await res.json()) as dataSource;
+export async function getDataSource({
+  workspaceId,
+  dataSourceId,
+}: {
+  workspaceId: number;
+  dataSourceId: number;
+}) {
+  const res = await fetchX(
+    `workspaces/${workspaceId}/data-sources/${dataSourceId}`,
+    {
+      method: 'GET',
+    },
+  );
+  return (await res.json()) as globalDataSource;
+}
+
+export async function getDataSourcesSameConnection({
+  workspaceId,
+  globalDataSourceId,
+}: {
+  workspaceId: number;
+  globalDataSourceId: number;
+}) {
+  const res = await fetchX(
+    `workspaces/${workspaceId}/data-sources/${globalDataSourceId}`,
+    {
+      method: 'GET',
+    },
+  );
+  return (await res.json()) as globalDataSource;
+}
+
+export async function addDataSource({
+  workspaceId,
+  dataSourceId,
+  data,
+}: {
+  workspaceId: string | undefined;
+  dataSourceId: number;
+  data: z.infer<typeof dataSourceMeta>;
+}) {
+  const res = await fetchX(
+    `workspaces/${workspaceId}/data-sources/${dataSourceId}`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    },
+  );
+  return (await res.json()) as Partial<dataSource>;
 }
 
 export async function updateDataSource({
@@ -86,6 +150,79 @@ export async function deleteDataSource({
     workspaceId: string;
   };
 }
+export async function deleteDataSourcesSameConnection({
+  workspaceId,
+  globalDataSourceId,
+}: {
+  workspaceId: string | undefined;
+  globalDataSourceId: string;
+}) {
+  const res = await fetchX(
+    `workspaces/${workspaceId}/data-sources/${globalDataSourceId}`,
+    {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
+  return (await res.json()) as {
+    dataSourceId: string;
+    deletedById: string;
+    workspaceId: string;
+  };
+}
+
+function useAddGlobalDataSource(
+  options?: UseMutationOptions<
+    Awaited<ReturnType<typeof addGlobalDataSource>>,
+    Error,
+    Parameters<typeof addGlobalDataSource>[0]
+  >,
+) {
+  const mutate = useMutation({
+    mutationFn: addGlobalDataSource,
+    ...options,
+  });
+  return mutate;
+}
+function useGlobalDataSources() {
+  return useQuery({
+    queryKey: ['globalDataSources'],
+    queryFn: () => getGlobalDataSources(),
+    staleTime: 0,
+  });
+}
+function useGlobalDataSource(id: number) {
+  return useQuery({
+    queryKey: ['globalDataSource', { id }],
+    queryFn: () => getGlobalDataSource(id),
+    staleTime: 0,
+  });
+}
+function useDataSources(workspaceId: number) {
+  return useQuery({
+    queryKey: ['globalDataSource', { workspaceId }],
+    queryFn: () => getDataSources({ workspaceId }),
+    staleTime: 0,
+  });
+}
+function useDataSource(workspaceId: number, dataSourceId: number) {
+  return useQuery({
+    queryKey: ['globalDataSource', { workspaceId, dataSourceId }],
+    queryFn: () => getDataSource({ workspaceId, dataSourceId }),
+    staleTime: 0,
+  });
+}
+function useDataSourcesSameConnection(
+  workspaceId: number,
+  globalDataSourceId: number,
+) {
+  return useQuery({
+    queryKey: ['globalDataSource', { workspaceId, globalDataSourceId }],
+    queryFn: () =>
+      getDataSourcesSameConnection({ workspaceId, globalDataSourceId }),
+    staleTime: 0,
+  });
+}
 
 function useUpdateDataSource(
   options?: UseMutationOptions<
@@ -101,26 +238,6 @@ function useUpdateDataSource(
   return mutate;
 }
 
-export async function addDataSource({
-  workspaceId,
-  dataSourceId,
-  data,
-}: {
-  workspaceId: string | undefined;
-  dataSourceId: string;
-  data: z.infer<typeof dataSourceMeta>;
-}) {
-  const res = await fetchX(
-    `workspaces/${workspaceId}/data-sources/${dataSourceId}`,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    },
-  );
-  return (await res.json()) as Partial<dataSource>;
-}
-
 function useAddDataSource(
   options?: UseMutationOptions<
     Awaited<ReturnType<typeof addDataSource>>,
@@ -134,3 +251,44 @@ function useAddDataSource(
   });
   return mutate;
 }
+function useDeleteDataSource(
+  options?: UseMutationOptions<
+    Awaited<ReturnType<typeof deleteDataSource>>,
+    Error,
+    Parameters<typeof deleteDataSource>[0]
+  >,
+) {
+  const mutate = useMutation({
+    mutationFn: deleteDataSource,
+    ...options,
+  });
+  return mutate;
+}
+function useDeleteDataSourcesSameConnection(
+  options?: UseMutationOptions<
+    Awaited<ReturnType<typeof deleteDataSourcesSameConnection>>,
+    Error,
+    Parameters<typeof deleteDataSourcesSameConnection>[0]
+  >,
+) {
+  const mutate = useMutation({
+    mutationFn: deleteDataSourcesSameConnection,
+    ...options,
+  });
+  return mutate;
+}
+
+export const globalDataSources = {
+  index: { useQuery: useGlobalDataSources },
+  one: { useQuery: useGlobalDataSource },
+  insert: { useMutation: useAddGlobalDataSource },
+};
+export const dataSources = {
+  index: { useQuery: useDataSources },
+  one: { useQuery: useDataSource },
+  sameConnection: { useQuery: useDataSourcesSameConnection },
+  insert: { useMutation: useAddDataSource },
+  update: { useMutation: useUpdateDataSource },
+  delete: { useMutation: useDeleteDataSource },
+  deleteConnections: { useMutation: useDeleteDataSourcesSameConnection },
+};
