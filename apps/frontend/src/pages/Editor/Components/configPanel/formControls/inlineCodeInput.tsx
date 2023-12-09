672178ac5d5c48cfefa726f4bd803f6e0238a866
zam <ahmed195138@feng.bu.edu.eg>
2023-12-09 21:11:05 +0200

    inlineCodeInput now scrollable

--- apps/frontend/src/pages/Editor/Components/configPanel/formControls/inlineCodeInput.tsx
+++ apps/frontend/src/pages/Editor/Components/configPanel/formControls/inlineCodeInput.tsx
@@ -3,19 +3,21 @@ import { InlineCodeInputProps } from '@webloom/configpaneltypes';
 import { WebloomInlineEditor } from '../../CodeEditor/inlineEditor';
 import { useContext } from 'react';
 import { FormControlContext } from '..';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 
 const InlineCodeInput = (props: InlineCodeInputProps) => {
   const { onChange } = useContext(FormControlContext);
   return (
     <div className="flex flex-col space-y-3">
       <Label>{props.label}</Label>
-
-      <WebloomInlineEditor
-        placeholder={props.placeholder}
-        value={props.value || ''}
-        onChange={onChange}
-        className="border-input bg-background ring-offset-background focus-visible:ring-ring min-h-10 w-full overflow-auto rounded-md border px-3 py-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-      />
+      <ScrollArea className="border-input bg-background ring-offset-background focus-visible:ring-ring min-h-10 w-full overflow-auto rounded-md border px-3 py-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
+        <WebloomInlineEditor
+          placeholder={props.placeholder}
+          value={props.value || ''}
+          onChange={onChange}
+        />
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   );
 };
