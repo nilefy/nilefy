a429b9adbb574aefca32914ca809d5e388d23d54
nagy nabil <nagynabil651@gmail.com>
2023-12-14 18:15:55 +0200

    fix(db schema): query name don't need to be unique across all apps, only unique locally

--- apps/backend/src/drizzle/schema/data_sources.schema.ts
+++ apps/backend/src/drizzle/schema/data_sources.schema.ts
@@ -59,19 +59,25 @@ export const workspaceDataSources = pgTable(
   },
 );
 
-export const queries = pgTable('workspace_app_queries', {
-  id: serial('id').primaryKey(),
-  name: varchar('query_name', { length: 100 }).unique().notNull(),
-  query: json('query').$type<Record<string, unknown>>().notNull(),
-  appId: integer('app_id')
-    .references(() => apps.id)
-    .notNull(),
-  dataSourceId: integer('data_source_id')
-    .references(() => workspaceDataSources.id, { onDelete: 'cascade' })
-    .notNull(),
-  createdById: integer('created_by_id')
-    .references(() => users.id)
-    .notNull(),
-  updatedById: integer('updated_by_id').references(() => users.id),
-  ...timeStamps,
-});
+export const queries = pgTable(
+  'workspace_app_queries',
+  {
+    id: serial('id').primaryKey(),
+    name: varchar('query_name', { length: 100 }).notNull(),
+    query: json('query').$type<Record<string, unknown>>().notNull(),
+    appId: integer('app_id')
+      .references(() => apps.id)
+      .notNull(),
+    dataSourceId: integer('data_source_id')
+      .references(() => workspaceDataSources.id, { onDelete: 'cascade' })
+      .notNull(),
+    createdById: integer('created_by_id')
+      .references(() => users.id)
+      .notNull(),
+    updatedById: integer('updated_by_id').references(() => users.id),
+    ...timeStamps,
+  },
+  (t) => ({
+    nameUnique: unique().on(t.name, t.appId),
+  }),
+);
