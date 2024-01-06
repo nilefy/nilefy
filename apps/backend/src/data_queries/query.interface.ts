c28a248c0ba2df3ddbd9bac979ec1c796f68fdf6
nagy nabil <nagynabil651@gmail.com>
2024-01-06 17:21:28 +0200

    chore(datasourecs): copy form config out of the seeder

--- apps/backend/src/data_queries/query.interface.ts
+++ apps/backend/src/data_queries/query.interface.ts
@@ -2,8 +2,8 @@ import { DataSourceConfigT } from '../dto/data_sources.dto';
 import { QueryConfig, QueryRet } from './query.types';
 
 export interface QueryRunnerI<
-  T = DataSourceConfigT,
-  U = Record<string, unknown>,
+  T extends DataSourceConfigT = DataSourceConfigT,
+  U extends Record<string, unknown> = Record<string, unknown>,
 > {
   run(dataSourceConfig: T, query: QueryConfig<U>): Promise<QueryRet>;
 
--- apps/backend/src/data_sources/plugins/postgresql/types.ts
+++ apps/backend/src/data_sources/plugins/postgresql/types.ts
@@ -1,3 +1,4 @@
+import { WidgetInspectorConfig } from '@webloom/configpaneltypes';
 import { z } from 'zod';
 
 export const configSchema = z.object({
@@ -15,3 +16,107 @@ export type ConfigT = z.infer<typeof configSchema>;
 export type QueryT = {
   query: string;
 };
+
+export const pluginConfigForm: WidgetInspectorConfig<ConfigT> = [
+  {
+    sectionName: 'Development',
+    children: [
+      {
+        id: 'host',
+        key: 'host',
+        label: 'Host',
+        type: 'input',
+        options: {
+          placeholder: 'localhost',
+          type: 'text',
+        },
+      },
+      {
+        id: 'port',
+        key: 'port',
+        label: 'Port',
+        type: 'input',
+        options: {
+          placeholder: 5432,
+          type: 'number',
+        },
+      },
+      {
+        id: 'ssl',
+        key: 'ssl',
+        label: 'SSL',
+        type: 'checkbox',
+        options: {},
+      },
+      {
+        id: 'database_name',
+        key: 'database',
+        label: 'Database Name',
+        type: 'input',
+        options: {
+          placeholder: 'Name of the database',
+          type: 'text',
+        },
+      },
+      {
+        id: 'username',
+        key: 'user',
+        label: 'Username',
+        type: 'input',
+        options: {
+          placeholder: 'Enter username',
+          type: 'text',
+        },
+      },
+      {
+        id: 'password',
+        key: 'password',
+        label: 'Password',
+        type: 'input',
+        options: {
+          placeholder: 'Enter password',
+          type: 'password',
+        },
+      },
+      {
+        id: 'certificate',
+        key: 'sslCertificate',
+        label: 'SSL Certificate',
+        type: 'select',
+        options: {
+          items: [
+            {
+              label: 'CA Certificate',
+              value: 'ca',
+            },
+            {
+              label: 'Self-signed Certificate',
+              value: 'self-signed',
+            },
+            {
+              label: 'None',
+              value: 'none',
+            },
+          ],
+          placeholder: 'None',
+        },
+      },
+      // TODO: add connection options key-value pairs
+    ],
+  },
+];
+
+export const queryConfigForm: WidgetInspectorConfig<QueryT> = [
+  {
+    sectionName: '',
+    children: [
+      {
+        id: 'sql',
+        key: 'query',
+        label: 'SQL',
+        type: 'sqlEditor',
+        options: { placeholder: 'select * from table;' },
+      },
+    ],
+  },
+];
