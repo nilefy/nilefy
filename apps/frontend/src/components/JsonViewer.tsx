9eefa498e529e7a2440e59cdbf22c8047c12b072
nagy nabil <nagynabil651@gmail.com>
2024-01-23 16:56:12 +0200

    refactor(jsonviewer): life - change colliding names(open, isOpen) - extract most of the duplicated code into functions - fix number of props on each node(previously was showing number of   nodes in the editor with each node, i changed it to the number of   props in the node) - fix scrollbar css in dark mode - fix "node in list needs key"

--- apps/frontend/src/components/JsonViewer.tsx
+++ apps/frontend/src/components/JsonViewer.tsx
@@ -16,97 +16,104 @@ type ElementProps = {
   [key: string]: unknown;
 };
 
-const trueTypeOf = (obj: unknown) =>
-  Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+function trueTypeOf(obj: unknown) {
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+}
 
-export function JsonViewer() {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const root = store((state) => state.tree[EDITOR_CONSTANTS.ROOT_NODE_ID]);
-  const initialState = root.nodes.reduce(
-    (acc, nodeId) => {
-      (acc as Record<string, boolean>)[nodeId] = false;
-      return acc;
-    },
-    {} as Record<string, boolean>,
+function CollapsibleTriggerWithChevr({ isOpen }: { isOpen: boolean }) {
+  return (
+    <CollapsibleTrigger asChild>
+      <Button variant="ghost" size="icon" className="w-9">
+        {isOpen ? (
+          <ChevronsDown className=" h-4 w-4" />
+        ) : (
+          <ChevronsRight className="h-4 w-4" />
+        )}
+      </Button>
+    </CollapsibleTrigger>
   );
-  const [open, setOpen] = React.useState(initialState);
+}
 
+function RecursiveProps({
+  parentKey,
+  elementProps,
+}: {
+  parentKey: string;
+  elementProps: ElementProps;
+}) {
   const [openPropsStates, setOpenPropsStates] = React.useState<
     Record<string, boolean>
   >({});
 
-  const recursiveProps = (props: ElementProps) => {
-    const collectedProps: JSX.Element[] = [];
-    for (const key in props) {
-      if (Object.prototype.hasOwnProperty.call(props, key)) {
-        const prop = props[key];
-        if (typeof prop === 'object' && prop != null) {
-          const isOpen = openPropsStates[key] || false;
-          collectedProps.push(
-            <Collapsible
-              className={`  p-0 font-mono text-sm`}
-              open={isOpen}
-              onOpenChange={(newOpenState) => {
-                setOpenPropsStates((prevState) => ({
-                  ...prevState,
-                  [key]: newOpenState, // Update the open state for this property
-                }));
-              }}
-            >
-              <div className="flex items-center">
-                <CollapsibleTrigger asChild>
-                  <Button variant="ghost" size="sm" className="-ml-9  w-9 p-0">
-                    {openPropsStates[key] ? (
-                      <ChevronsDown className=" h-4 w-4" />
-                    ) : (
-                      <ChevronsRight className="h-4 w-4" />
-                    )}
-                  </Button>
-                </CollapsibleTrigger>
-                <h4 className="text-sm font-semibold">{key}</h4>
-                <p className="ml-2 text-xs">
-                  {trueTypeOf(prop)} {Object.keys(prop!).length} entries
-                </p>
-              </div>
-              <CollapsibleContent className="-ml-5 space-y-0 border-l-2 pl-10">
-                {recursiveProps(prop as ElementProps)}
-              </CollapsibleContent>
-            </Collapsible>,
-          );
-        } else {
-          // Display the non-object property
-          collectedProps.push(
-            <div key={key} className="">
-              <p className="inline text-xs">{key} </p>
-              <p className="inline text-xs text-orange-600">
-                &quot;{String(prop)}&quot;
-              </p>
-              <p className="ml-2 inline text-xs">{trueTypeOf(prop)}</p>
-            </div>,
-          );
-        }
-      }
+  const collectedProps: JSX.Element[] = [];
+
+  for (const key in elementProps) {
+    const prop = elementProps[key];
+    if (typeof prop === 'object' && prop != null) {
+      const isOpen = openPropsStates[key] || false;
+      collectedProps.push(
+        <Collapsible
+          key={parentKey + key}
+          className="p-0 font-mono text-sm"
+          open={isOpen}
+          onOpenChange={(newOpenState) => {
+            setOpenPropsStates((prevState) => ({
+              ...prevState,
+              [key]: newOpenState, // Update the open state for this property
+            }));
+          }}
+        >
+          <div className="flex items-center">
+            <CollapsibleTriggerWithChevr isOpen={openPropsStates[key]} />
+            <h4 className="text-sm font-semibold">{key}</h4>
+            <p className="ml-2 text-xs">
+              {trueTypeOf(prop)} {Object.keys(prop).length} entries
+            </p>
+          </div>
+          <CollapsibleContent className="-ml-5 space-y-0 border-l-2 pl-10">
+            <RecursiveProps
+              parentKey={parentKey + key}
+              elementProps={prop as ElementProps}
+            />
+          </CollapsibleContent>
+        </Collapsible>,
+      );
+    } else {
+      // Display the non-object property
+      collectedProps.push(
+        <div key={key} className="">
+          <p className="inline text-xs">{key} </p>
+          <p className="inline text-xs text-orange-600">
+            &quot;{String(prop)}&quot;
+          </p>
+          <p className="ml-2 inline text-xs">{trueTypeOf(prop)}</p>
+        </div>,
+      );
     }
-    return collectedProps;
-  };
+  }
+  return collectedProps;
+}
+
+/**
+ * don't let the name mislead you
+ * this component can only works with editor state
+ */
+export function JsonViewer() {
+  const [isComponentsOpen, setIsComponentsOpen] = React.useState(false);
+  const tree = store((state) => state.tree);
+  const root = tree[EDITOR_CONSTANTS.ROOT_NODE_ID];
+  const [isSingleNodeOpen, setIsSingleNodeOpen] = React.useState<
+    Record<string, boolean>
+  >({});
 
   return (
     <Collapsible
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      className="h-full w-full space-y-0 overflow-y-auto font-mono scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-gray-700 dark:scrollbar-track-white dark:scrollbar-thumb-gray-700"
+      open={isComponentsOpen}
+      onOpenChange={setIsComponentsOpen}
+      className="h-full w-full space-y-0 overflow-y-auto font-mono scrollbar-thin scrollbar-track-foreground/10 scrollbar-thumb-primary/10"
     >
       <div className="flex items-center">
-        <CollapsibleTrigger asChild>
-          <Button variant="ghost" size="sm" className="w-9 p-0">
-            {isOpen ? (
-              <ChevronsDown className="h-4 w-4" />
-            ) : (
-              <ChevronsRight className="h-4 w-4" />
-            )}
-            <span className="sr-only">Toggle</span>
-          </Button>
-        </CollapsibleTrigger>
+        <CollapsibleTriggerWithChevr isOpen={isComponentsOpen} />
         <h4 className="text-sm font-semibold">Components</h4>
         <p className="ml-4 text-xs">
           {typeof root} {root.nodes.length} entries
@@ -114,30 +121,23 @@ export function JsonViewer() {
       </div>
       <CollapsibleContent className="ml-4 space-y-2 border-l-2">
         {root.nodes.map((nodeId) => {
-          const node = store.getState().tree[nodeId];
+          const node = tree[nodeId];
           return (
             <Collapsible
               key={node.id}
-              open={open[nodeId]}
+              open={isSingleNodeOpen[nodeId] || false}
               onOpenChange={(newOpenState) => {
-                setOpen((prevState) => ({
+                setIsSingleNodeOpen((prevState) => ({
                   ...prevState,
                   [nodeId]: newOpenState,
                 }));
               }}
-              className={`  p-0 font-mono text-sm`}
+              className="p-0 font-mono text-sm"
             >
               <div className="flex items-center justify-start space-x-2 px-0">
-                <CollapsibleTrigger asChild>
-                  <Button variant="ghost" size="sm" className="w-9 p-0">
-                    {open[nodeId] ? (
-                      <ChevronsDown className="h-4 w-4" />
-                    ) : (
-                      <ChevronsRight className="h-4 w-4" />
-                    )}
-                  </Button>
-                </CollapsibleTrigger>
-
+                <CollapsibleTriggerWithChevr
+                  isOpen={isSingleNodeOpen[nodeId] || false}
+                />
                 <h4 className="text-sm font-semibold">
                   <button
                     onClick={() =>
@@ -150,12 +150,12 @@ export function JsonViewer() {
                   </button>
                 </h4>
                 <p className="ml-2 text-xs">
-                  {typeof node} {root.nodes.length} entries
+                  {typeof node} {Object.keys(node.props).length} entries
                 </p>
               </div>
 
               <CollapsibleContent className="ml-4 space-y-0 border-l-2 pl-8">
-                {recursiveProps(node.props)}
+                <RecursiveProps elementProps={node.props} parentKey={node.id} />
               </CollapsibleContent>
             </Collapsible>
           );
