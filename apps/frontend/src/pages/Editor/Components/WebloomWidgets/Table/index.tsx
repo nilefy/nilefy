cd300fa4d315bd06599961c321d7ba242a572c8a
zam <ahmed195138@feng.bu.edu.eg>
2024-02-20 16:46:05 +0200

    adapted table to mobx

--- apps/frontend/src/pages/Editor/Components/WebloomWidgets/Table/index.tsx
+++ apps/frontend/src/pages/Editor/Components/WebloomWidgets/Table/index.tsx
@@ -39,7 +39,7 @@ import { editorStore } from '@/lib/Editor/Models';
 import { observer } from 'mobx-react-lite';
 import zodToJsonSchema from 'zod-to-json-schema';
 import { z } from 'zod';
-import { toJS } from 'mobx';
+import { autorun, toJS } from 'mobx';
 type RowData = Record<string, unknown>;
 
 export type WebLoomTableColumn = {
@@ -59,11 +59,12 @@ export type WebloomTableProps = {
 };
 
 // Helper function to generate columns from data
-const generateColumnsFromData = (data: RowData[]): WebLoomTableColumn[] => {
-  if (data.length === 0) {
-    return [];
-  }
-  return Object.keys(data[0]).map((key, index) => {
+const generateColumnsFromData = (
+  data: RowData | undefined,
+): WebLoomTableColumn[] => {
+  if (!data) return [];
+
+  return Object.keys(data).map((key, index) => {
     return {
       id: (index + 1).toString(),
       accessorKey: key,
@@ -78,217 +79,215 @@ const WebloomTable = observer(() => {
   const { onPropChange, id } = useContext(WidgetContext);
   const props = editorStore.currentPage.getWidgetById(id)
     .finalValues as WebloomTableProps;
-  console.log('props', toJS(props));
-  // const {
-  //   data = [],
-  //   columns = [],
-  //   isRowSelectionEnabled,
-  //   isSearchEnabled,
-  //   isPaginationEnabled,
-  // } = props;
 
-  // //  to run only once when the component is mounted
-  // React.useEffect(() => {
-  //   // merging predefined cols and cols generated from data
-  //   const cols = generateColumnsFromData(data);
-  //   cols.forEach((propCol) => {
-  //     const exists = columns.find((col) => col.id === propCol.id);
-  //     if (!exists) {
-  //       columns.push(propCol);
-  //     }
-  //   });
+  //  to run only once when the component is mounted
+  React.useEffect(
+    () =>
+      autorun(() => {
+        console.log('autorun');
+        const columns = generateColumnsFromData(props.data[0]);
+        onPropChange({
+          key: 'columns',
+          value: columns,
+        });
+      }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
 
-  //   onPropChange({ key: 'columns', value: cols });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [onPropChange, data]);
+  // pagination options
+  const [{ pageIndex }, setPagination] = React.useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: props.isPaginationEnabled
+      ? props.pageSize ?? 3
+      : props.data.length,
+  });
+  const pagination = React.useMemo(
+    () => ({
+      pageIndex,
+      pageSize: props.isPaginationEnabled
+        ? props.pageSize ?? 3
+        : props.data.length,
+    }),
+    [pageIndex, props.isPaginationEnabled, props.pageSize, props.data.length],
+  );
 
-  // // pagination options
-  // const [{ pageIndex }, setPagination] = React.useState<PaginationState>({
-  //   pageIndex: 0,
-  //   pageSize: isPaginationEnabled ? props.pageSize ?? 3 : data.length,
-  // });
-  // const pagination = React.useMemo(
-  //   () => ({
-  //     pageIndex,
-  //     pageSize: isPaginationEnabled ? props.pageSize ?? 3 : data.length,
-  //   }),
-  //   [pageIndex, isPaginationEnabled, props.pageSize, data.length],
-  // );
+  // sorting options
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  // filtering options
+  const [globalFilter, setGlobalFilter] = React.useState('');
+  // selection options
+  const [rowSelection, setRowSelection] = React.useState({});
 
-  // // sorting options
-  // const [sorting, setSorting] = React.useState<SortingState>([]);
-  // // filtering options
-  // const [globalFilter, setGlobalFilter] = React.useState('');
-  // // selection options
-  // const [rowSelection, setRowSelection] = React.useState({});
+  // mapping the columns to be  compatible with tanstack-table
 
-  // // mapping the columns to be  compatible with tanstack-table
+  const tableCols =
+    props.columns?.map((col) => {
+      return {
+        id: col.id,
+        accessorKey: col.accessorKey,
+        header: ({ column }: { column: Column<RowData> }) => {
+          return (
+            <Button
+              variant="ghost"
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === 'asc')
+              }
+            >
+              {col.accessorKey.toUpperCase()}
+              <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+          );
+        },
+      };
+    }) || [];
 
-  // const tableCols = columns.map((col) => {
-  //   return {
-  //     id: col.id,
-  //     accessorKey: col.accessorKey,
-  //     header: ({ column }: { column: Column<RowData> }) => {
-  //       return (
-  //         <Button
-  //           variant="ghost"
-  //           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-  //         >
-  //           {col.accessorKey.toUpperCase()}
-  //           <ArrowUpDown className="ml-2 h-4 w-4" />
-  //         </Button>
-  //       );
-  //     },
-  //   };
-  // });
+  const table = useReactTable({
+    data: props.data,
+    columns: props.isRowSelectionEnabled
+      ? [
+          {
+            id: 'select',
+            header: ({ table }) => (
+              <Checkbox
+                checked={
+                  table.getIsAllPageRowsSelected() ||
+                  (table.getIsSomePageRowsSelected() && 'indeterminate')
+                }
+                onCheckedChange={(value) =>
+                  table.toggleAllPageRowsSelected(!!value)
+                }
+                aria-label="Select all"
+              />
+            ),
+            cell: ({ row }) => (
+              <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+              />
+            ),
+            enableSorting: false,
+            enableHiding: false,
+          },
+          ...tableCols,
+        ]
+      : tableCols,
+    state: { pagination, rowSelection, sorting, globalFilter },
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onRowSelectionChange: setRowSelection,
+  });
 
-  // const table = useReactTable({
-  //   data,
-  //   columns: isRowSelectionEnabled
-  //     ? [
-  //         {
-  //           id: 'select',
-  //           header: ({ table }) => (
-  //             <Checkbox
-  //               checked={
-  //                 table.getIsAllPageRowsSelected() ||
-  //                 (table.getIsSomePageRowsSelected() && 'indeterminate')
-  //               }
-  //               onCheckedChange={(value) =>
-  //                 table.toggleAllPageRowsSelected(!!value)
-  //               }
-  //               aria-label="Select all"
-  //             />
-  //           ),
-  //           cell: ({ row }) => (
-  //             <Checkbox
-  //               checked={row.getIsSelected()}
-  //               onCheckedChange={(value) => row.toggleSelected(!!value)}
-  //               aria-label="Select row"
-  //             />
-  //           ),
-  //           enableSorting: false,
-  //           enableHiding: false,
-  //         },
-  //         ...tableCols,
-  //       ]
-  //     : tableCols,
-  //   state: { pagination, rowSelection, sorting, globalFilter },
-  //   onGlobalFilterChange: setGlobalFilter,
-  //   getFilteredRowModel: getFilteredRowModel(),
-  //   onSortingChange: setSorting,
-  //   getSortedRowModel: getSortedRowModel(),
-  //   onPaginationChange: setPagination,
-  //   getCoreRowModel: getCoreRowModel(),
-  //   getPaginationRowModel: getPaginationRowModel(),
-  //   onRowSelectionChange: setRowSelection,
-  // });
-
-  // return (
-  //   <div className="flex h-full w-full flex-col overflow-auto">
-  //     {isSearchEnabled && (
-  //       <div className=" ml-auto  w-[40%] p-2">
-  //         <Input
-  //           value={globalFilter ?? ''}
-  //           onChange={(event) => setGlobalFilter(String(event.target.value))}
-  //           className=" border p-2 shadow"
-  //           placeholder="Search"
-  //         />
-  //       </div>
-  //     )}
-  //     <div className="h-full w-full rounded-md border shadow-md">
-  //       <Table className="h-full">
-  //         <TableHeader>
-  //           {table.getHeaderGroups().map((headerGroup) => (
-  //             <TableRow key={headerGroup.id}>
-  //               {headerGroup.headers.map((header) => {
-  //                 return (
-  //                   <TableHead key={header.id}>
-  //                     {header.isPlaceholder
-  //                       ? null
-  //                       : flexRender(
-  //                           header.column.columnDef.header,
-  //                           header.getContext(),
-  //                         )}
-  //                   </TableHead>
-  //                 );
-  //               })}
-  //             </TableRow>
-  //           ))}
-  //         </TableHeader>
-  //         <TableBody>
-  //           {table.getRowModel().rows.map((row) => (
-  //             <TableRow key={row.id}>
-  //               {row.getVisibleCells().map((cell) => (
-  //                 <TableCell key={cell.id}>
-  //                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
-  //                 </TableCell>
-  //               ))}
-  //             </TableRow>
-  //           ))}
-  //         </TableBody>
-  //       </Table>
-  //     </div>
-  //     {isPaginationEnabled && (
-  //       <div className="flex items-center justify-center space-x-2 py-4">
-  //         <Button
-  //           variant="ghost"
-  //           className="rounded border p-1"
-  //           onClick={() => table.setPageIndex(0)}
-  //           disabled={!table.getCanPreviousPage()}
-  //         >
-  //           <ChevronsLeft />
-  //         </Button>
-  //         <Button
-  //           variant="ghost"
-  //           size="sm"
-  //           onClick={() => table.previousPage()}
-  //           disabled={!table.getCanPreviousPage()}
-  //         >
-  //           <MoveLeft />
-  //         </Button>
-  //         <Button
-  //           variant="ghost"
-  //           size="sm"
-  //           onClick={() => table.nextPage()}
-  //           disabled={!table.getCanNextPage()}
-  //         >
-  //           <MoveRight />
-  //         </Button>
-  //         <Button
-  //           variant="ghost"
-  //           className="rounded border p-1"
-  //           onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-  //           disabled={!table.getCanNextPage()}
-  //         >
-  //           <ChevronsRight />
-  //         </Button>
-  //         <span className="flex items-center gap-1">
-  //           <div>Page</div>
-  //           <strong>
-  //             {table.getState().pagination.pageIndex + 1} of{' '}
-  //             {table.getPageCount()}
-  //           </strong>
-  //         </span>
-  //         <span className="flex items-center gap-1">
-  //           | Go to page:
-  //           <Input
-  //             type="number"
-  //             min={1}
-  //             max={table.getPageCount()}
-  //             defaultValue={table.getState().pagination.pageIndex + 1}
-  //             onChange={(e) => {
-  //               const page = e.target.value ? Number(e.target.value) - 1 : 0;
-  //               table.setPageIndex(page);
-  //             }}
-  //             className="w-16 rounded border p-1"
-  //           />
-  //         </span>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-  return <div>Table</div>;
+  return (
+    <div className="flex h-full w-full flex-col overflow-auto">
+      {props.isSearchEnabled && (
+        <div className=" ml-auto  w-[40%] p-2">
+          <Input
+            value={globalFilter ?? ''}
+            onChange={(event) => setGlobalFilter(String(event.target.value))}
+            className=" border p-2 shadow"
+            placeholder="Search"
+          />
+        </div>
+      )}
+      <div className="h-full w-full rounded-md border shadow-md">
+        <Table className="h-full">
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      {props.isPaginationEnabled && (
+        <div className="flex items-center justify-center space-x-2 py-4">
+          <Button
+            variant="ghost"
+            className="rounded border p-1"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <ChevronsLeft />
+          </Button>
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <MoveLeft />
+          </Button>
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <MoveRight />
+          </Button>
+          <Button
+            variant="ghost"
+            className="rounded border p-1"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <ChevronsRight />
+          </Button>
+          <span className="flex items-center gap-1">
+            <div>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of{' '}
+              {table.getPageCount()}
+            </strong>
+          </span>
+          <span className="flex items-center gap-1">
+            | Go to page:
+            <Input
+              type="number"
+              min={1}
+              max={table.getPageCount()}
+              defaultValue={table.getState().pagination.pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                table.setPageIndex(page);
+              }}
+              className="w-16 rounded border p-1"
+            />
+          </span>
+        </div>
+      )}
+    </div>
+  );
 });
 
 const config: WidgetConfig = {
