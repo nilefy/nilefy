0867627de825c72513bf534f7ec1b6201db3c348
nagy nabil <nagynabil651@gmail.com>
2024-02-23 17:37:35 +0200

    styles: arrayFieldItem styles update

--- apps/frontend/src/components/rjsf_shad/arrayFieldItemTemplate.tsx
+++ apps/frontend/src/components/rjsf_shad/arrayFieldItemTemplate.tsx
@@ -8,32 +8,76 @@ import {
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
 import { WidgetsEventHandler } from './eventHandler';
 import { ReactElement } from 'react';
+import { Copy, MoreVertical, Trash } from 'lucide-react';
 
 export enum ArrayFieldItemType {
   'EventHandlerItem' = 1,
 }
 
+type EventHandlerItemUtilsProps = {
+  onCopyCB: () => void;
+  onDeleteCB: () => void;
+};
+
+function EventHandlerItemUtils({
+  onCopyCB,
+  onDeleteCB,
+}: EventHandlerItemUtilsProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <MoreVertical />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {/*
+          CLONE
+          */}
+        <DropdownMenuItem
+          onClick={() => {
+            console.log('here');
+            onCopyCB();
+          }}
+        >
+          <Copy className="mr-2 h-4 w-4" />
+          <span>Duplicate</span>
+        </DropdownMenuItem>
+
+        {/*DELETE*/}
+        <DropdownMenuItem className="text-red-500" onClick={onDeleteCB}>
+          <Trash className="mr-2 h-4 w-4" />
+          <span>Delete</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
 function EventHandlerItemView({
   event,
   children,
+  onDeleteCB,
+  onCopyCB,
 }: {
   event: WidgetsEventHandler[0];
   children: ReactElement;
-}) {
+} & EventHandlerItemUtilsProps) {
   return (
     <DropdownMenu>
-      <div className="flex h-full w-full justify-between">
-        <DropdownMenuTrigger className="h-full w-full">
-          <p className="flex min-h-full w-full min-w-full gap-3 border-black">
-            {/*TODO: edit span className*/}
-            <span className="">{event.type}</span>
-            <span className="">{event.config.type}</span>
+      <div className="flex h-full w-full justify-between rounded-2xl  border-2 p-3">
+        <DropdownMenuTrigger className="h-full w-full ">
+          <p className="line-clamp-3 flex min-h-full w-full min-w-full items-center gap-3 ">
+            <span className="w-fit rounded-2xl bg-secondary p-3">
+              {event.type ?? 'unconfigured'}
+            </span>
+            <span className="">{event.config.type ?? 'unconfigured'}</span>
           </p>
         </DropdownMenuTrigger>
+        <EventHandlerItemUtils onCopyCB={onCopyCB} onDeleteCB={onDeleteCB} />
       </div>
 
       <DropdownMenuContent side="left" className="space-y-4 p-4">
@@ -50,15 +94,21 @@ function Item({
   itemType,
   itemValue,
   children,
+  onDeleteCB,
+  onCopyCB,
 }: {
   itemValue: any;
   children: ReactElement;
   itemType?: ArrayFieldItemType;
-}) {
+} & EventHandlerItemUtilsProps) {
   switch (itemType) {
     case ArrayFieldItemType.EventHandlerItem: {
       return (
-        <EventHandlerItemView event={itemValue}>
+        <EventHandlerItemView
+          event={itemValue}
+          onDeleteCB={onDeleteCB}
+          onCopyCB={onCopyCB}
+        >
           {children}
         </EventHandlerItemView>
       );
@@ -108,10 +158,16 @@ export default function ArrayFieldItemTemplate<
 
   return (
     <div className="flex h-full w-full items-center gap-2">
-      <Item itemValue={itemValue} itemType={customItemType}>
+      <Item
+        itemValue={itemValue}
+        itemType={customItemType}
+        onDeleteCB={onDropIndexClick(index)}
+        onCopyCB={onCopyIndexClick(index)}
+      >
         {children}
       </Item>
-      {hasToolbar && (
+
+      {hasToolbar && customItemType === undefined && (
         <div className="ml-auto flex items-center gap-3">
           {(hasMoveUp || hasMoveDown) && (
             <MoveUpButton
