11bc0a8c2d45dedb81e8288cff675308b53195e9
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2024-02-24 16:33:41 +0200

    datasource(azure-blob): add azure blob storage query service and seeder configs

--- apps/backend/src/data_sources/plugins/common/service.ts
+++ apps/backend/src/data_sources/plugins/common/service.ts
@@ -2,6 +2,7 @@ import { BadRequestException } from '@nestjs/common';
 import { QueryRunnerI } from '../../../data_queries/query.interface';
 import PostgresqlQueryService from '../postgresql/main';
 import RESTQueryService from '../restapi/main';
+import AzureBlobStorageQueryService from '../azure_blob_storage/main';
 
 export const getQueryService = (name: string): QueryRunnerI => {
   switch (name.toLowerCase()) {
@@ -9,6 +10,8 @@ export const getQueryService = (name: string): QueryRunnerI => {
       return new PostgresqlQueryService();
     case 'rest api':
       return new RESTQueryService();
+    case 'azure blob storage':
+      return new AzureBlobStorageQueryService();
     default:
       throw new BadRequestException();
   }
