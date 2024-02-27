9a0bc2957c779924e3d9679a500d58aa3ebdebea
Mohamedhadhoud1 <muhammed195837@feng.bu.edu.eg>
2024-02-27 18:44:17 +0200

    popper on progress

--- apps/frontend/src/pages/Editor/Components/lib/WebloomRoot.tsx
+++ apps/frontend/src/pages/Editor/Components/lib/WebloomRoot.tsx
@@ -4,14 +4,17 @@ import { EDITOR_CONSTANTS } from '@webloom/constants';
 import { useEffect, useLayoutEffect, useRef } from 'react';
 import { useSetDom } from '@/hooks';
 import { observer } from 'mobx-react-lite';
+import { usePopper } from 'react-popper';
+import { Trash2 } from 'lucide-react';
+import { DeleteAction } from '@/Actions/Editor/Delete';
+import { commandManager } from '@/Actions/CommandManager';
 import {
   createPopperLite as createPopper,
   preventOverflow,
   flip,
 } from '@popperjs/core';
-import { Trash2 } from 'lucide-react';
-import { DeleteAction } from '@/Actions/Editor/Delete';
-import { commandManager } from '@/Actions/CommandManager';
+import React, { useState } from 'react';
+import { Example } from './popper';
 export const WebloomRoot = observer(function WebloomRoot() {
   const root = editorStore.currentPage.rootWidget;
   const nodes = root.nodes;
@@ -59,22 +62,13 @@ export const WebloomRoot = observer(function WebloomRoot() {
     ? editorStore.currentPage.getWidgetById(selectedNode).pixelDimensions
     : null;
 
-  useEffect(() => {
-    createPopper(
-      //@ts-expect-error bla
-      document.querySelector(`[data-id="${selectedNode}"]`),
-      document.querySelector(`#${selectedNode}`),
-      {
-        placement: 'top',
-        modifiers: [preventOverflow, flip],
-      },
-    );
-  }, [selectedNode, dims?.y]);
+  
   return (
     <div id="webloom-root" className="relative h-screen w-full" ref={ref}>
+      <Example/>
       <WebloomAdapter droppable id={EDITOR_CONSTANTS.ROOT_NODE_ID}>
         <Grid id={EDITOR_CONSTANTS.ROOT_NODE_ID} />
-        {selectedNodeParent == EDITOR_CONSTANTS.ROOT_NODE_ID && (
+        {/* {selectedNodeParent == EDITOR_CONSTANTS.ROOT_NODE_ID && (
           <WebloomAdapter draggable droppable overflow id={selectedNode}>
             <div
               id={selectedNode}
@@ -82,14 +76,14 @@ export const WebloomRoot = observer(function WebloomRoot() {
               key={selectedNode}
               className="!left-0  h-5 w-full text-sm text-white"
             >
-              <div className="bg-blue-500 w-20 flex items-center justify-between">
+              <div className="flex w-20 items-center justify-between bg-blue-500">
                 <p>{selectedNode}</p>
 
                 <Trash2 size={16} />
               </div>
             </div>
           </WebloomAdapter>
-        )}
+        )} */}
         {nodes.map((node) => (
           <>
             <WebloomElement id={node} key={node} />
