e29afa5f6a37120a3baee3b0866e73ac064c1364
mhosnyessa <mhosny67710@gmail.com>
2024-03-02 01:25:49 +0200

    feat(confirm email): (moved-- todo -> useConfirmEmail )

---
+++ apps/frontend/src/api/confirm_email.api.tsx
@@ -0,0 +1,39 @@
+import { fetchX } from '@/utils/fetch';
+import { useQuery } from '@tanstack/react-query';
+
+export const confirmEmail = async (
+  confirmationData: EmailConfirmationData,
+): Promise<ConfirmationResponse> => {
+  console.log('right before the call');
+  const response = await fetchX(
+    `auth/confirm/${confirmationData.email}/${confirmationData.jwt}`,
+    {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
+
+  console.log('Right after the call');
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+export interface EmailConfirmationData {
+  email: string | undefined;
+  jwt: string | undefined;
+}
+
+interface ConfirmationResponse {
+  message: string; // You can adjust this according to the response structure from your backend
+}
+
+export function useConfirmEmail(email: string, token: string) {
+  const pages = useQuery({
+    // todo fix this
+    // todo edit email_confimration.tsx
+    queryKey: [PAGES_QUERY_KEY, { workspaceId, appId }],
+    queryFn: async () => await fetchPages({ workspaceId, appId }),
+    staleTime: 0,
+  });
+  return pages;
+}
--- apps/frontend/src/pages/auth/email_confirmation.tsx
+++ apps/frontend/src/pages/auth/email_confirmation.tsx
@@ -1,36 +1,16 @@
 import { Link, useParams } from 'react-router-dom';
 import { useAuthStore } from '@/hooks/useAuthStore';
-import { EmailConfirmationData, confirmEmail } from '@/api/auth';
+import { EmailConfirmationData, confirmEmail } from '@/api/confirm_email.api';
 
 export function EmailConfirmation() {
   const { email, token } = useParams();
-
-  // const confirmEmail = async (email: string, token: string) => {
-  //   try {
-  //     // Make the API call to your backend to confirm the email
-  //     const res = await fetchX(`/api/confirm-email/${email}/${token}`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json;charset=utf-8',
-  //       },
-  //       body: JSON.stringify({ email, token }),
-  //     });
-  //     console.log(await res.json()); // Handle response as needed
-  //   } catch (error) {
-  //     console.error('Error confirming email:', error);
-  //   }
-  // };
-
-  // // Check if user is authenticated and perform API call
 
   const emailConfigurationData: EmailConfirmationData = {
     email: email,
     jwt: token,
   };
 
-  // if (isAuthed && email && token) {
   confirmEmail(emailConfigurationData);
-  // }
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
