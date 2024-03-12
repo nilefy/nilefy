3686028aa75f579b579020b8a579619e90ed5ba7
nagy nabil <nagynabil651@gmail.com>
2024-03-12 05:59:04 +0200

    chore(backend): add connection test to postgres and created endpoint for connection test

--- apps/backend/src/data_queries/query.interface.ts
+++ apps/backend/src/data_queries/query.interface.ts
@@ -1,6 +1,16 @@
 import { DataSourceConfigT } from '../dto/data_sources.dto';
 import { QueryConfig, QueryRet } from './query.types';
 
+export type TestConnectionT = Promise<{
+  /**
+   * connection state
+   */
+  connected: boolean;
+  /**
+   * if the plugin wants to return message with the connection test result
+   */
+  msg?: string;
+}>;
 export interface QueryRunnerI<
   T extends DataSourceConfigT = DataSourceConfigT,
   U extends Record<string, unknown> = Record<string, unknown>,
@@ -18,5 +28,5 @@ export interface QueryRunnerI<
 
   connect?(dataSourceConfig: T): any;
 
-  testConnection?: (dataSourceConfig: T) => Promise<boolean>;
+  testConnection?: (dataSourceConfig: T) => TestConnectionT;
 }
