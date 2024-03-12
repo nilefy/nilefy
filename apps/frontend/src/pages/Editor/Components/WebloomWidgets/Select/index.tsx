ea402b8da64a9f169137605b32b17b2b4e694998
nagy nabil <nagynabil651@gmail.com>
2024-03-12 03:55:34 +0200

    chore(widgets): update select widget added the following - setValue setter - clearValue method - events handlers    - onOptionChange    - onOpenChange - tooltip prop - disabled prop - placeholder prop

--- apps/frontend/src/pages/Editor/Components/WebloomWidgets/Select/index.tsx
+++ apps/frontend/src/pages/Editor/Components/WebloomWidgets/Select/index.tsx
@@ -1,32 +1,106 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { Widget, WidgetConfig, selectOptions } from '@/lib/Editor/interface';
 import { CheckSquare } from 'lucide-react';
 import { WidgetInspectorConfig } from '@/lib/Editor/interface';
-import { useContext } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { WidgetContext } from '../..';
 import { editorStore } from '@/lib/Editor/Models';
 import { observer } from 'mobx-react-lite';
-import SelectComponent from './Select';
 import { Label } from '@/components/ui/label';
+import { ToolTipWrapper } from '../tooltipWrapper';
+import {
+  WidgetsEventHandler,
+  genEventHandlerUiSchema,
+  widgetsEventHandlerJsonSchema,
+} from '@/components/rjsf_shad/eventHandler';
 
 export type WebloomSelectProps = {
   options: selectOptions[];
   label: string;
-  value: string;
+  value?: string;
+  disabled?: boolean;
+  tooltip?: string;
+  placeholder?: string;
+  events: WidgetsEventHandler;
 };
 
-const WebloomSelect = observer(() => {
+// https://www.radix-ui.com/primitives/docs/components/select#root
+const webloomSelectEvents = {
+  onOptionChange: 'onOptionChange',
+  onOpenChange: 'onOpenChange',
+} as const;
+
+const WebloomSelect = observer(function WebloomSelect() {
   const { id, onPropChange } = useContext(WidgetContext);
-  const props = editorStore.currentPage.getWidgetById(id)
-    .finalValues as WebloomSelectProps;
+  const widget = editorStore.currentPage.getWidgetById(id);
+  const props = widget.finalValues as WebloomSelectProps;
+
+  const clearValue = useCallback(() => {
+    onPropChange({
+      key: 'value',
+      value: undefined,
+    });
+  }, [onPropChange]);
+
+  // append runtime methods
+  useEffect(() => {
+    widget.appendSetters([
+      {
+        key: 'clearValue',
+        setter: clearValue,
+      },
+    ]);
+  }, [clearValue]);
+
+  /**
+   * why do i set the key to this weird `id + props.value`?
+   * when clearValue is triggred but the value was set before radix select don't re-show the placeholder
+   * the workaround is to make react re-render the component when value changes from string back to undefined, so the component show the placeholder
+   * @link https://github.com/radix-ui/primitives/issues/1569
+   */
   return (
-    <div className="w-full">
-      <Label>{props.label}</Label>
-      <SelectComponent
-        value={props.value}
-        options={props.options}
-        onPropChange={onPropChange}
-      />
-    </div>
+    <ToolTipWrapper text={props.tooltip} key={id + props.value}>
+      <div className="w-full p-1">
+        <Label>{props.label}</Label>
+        <Select
+          value={props.value}
+          disabled={props.disabled}
+          onValueChange={(e) => {
+            onPropChange({
+              key: 'value',
+              value: e,
+            });
+            editorStore.executeActions<typeof webloomSelectEvents>(
+              id,
+              'onOptionChange',
+            );
+          }}
+          onOpenChange={() => {
+            editorStore.executeActions<typeof webloomSelectEvents>(
+              id,
+              'onOpenChange',
+            );
+          }}
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder={props.placeholder} />
+          </SelectTrigger>
+          <SelectContent>
+            {props.options.map((option: selectOptions) => (
+              <SelectItem key={option.value} value={option.value}>
+                {option.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+    </ToolTipWrapper>
   );
 });
 
@@ -50,7 +124,7 @@ const defaultProps: WebloomSelectProps = {
     { value: 'Option 3', label: 'Option 3' },
   ],
   label: 'Select',
-  value: 'Option 1',
+  events: [],
 };
 
 const schema: WidgetInspectorConfig = {
@@ -60,6 +134,9 @@ const schema: WidgetInspectorConfig = {
       label: {
         type: 'string',
         default: defaultProps.label,
+      },
+      placeholder: {
+        type: 'string',
       },
       options: {
         type: 'array',
@@ -75,6 +152,9 @@ const schema: WidgetInspectorConfig = {
           },
         },
       },
+      tooltip: { type: 'string' },
+      disabled: { type: 'boolean', default: false },
+      events: widgetsEventHandlerJsonSchema,
       value: {
         type: 'string',
       },
@@ -91,6 +171,16 @@ const schema: WidgetInspectorConfig = {
     options: {
       'ui:widget': 'inlineCodeInput',
     },
+    disabled: {
+      'ui:widget': 'inlineCodeInput',
+    },
+    tooltip: {
+      'ui:widget': 'inlineCodeInput',
+    },
+    placeholder: {
+      'ui:widget': 'inlineCodeInput',
+    },
+    events: genEventHandlerUiSchema(webloomSelectEvents),
   },
 };
 
@@ -99,6 +189,12 @@ export const WebloomSelectWidget: Widget<WebloomSelectProps> = {
   config,
   defaultProps,
   schema,
+  setters: {
+    setValue: {
+      path: 'value',
+      type: 'string',
+    },
+  },
 };
 
 export { WebloomSelect };
