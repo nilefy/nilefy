baed36c2268bbd24e8d29b3f85f1cf13d2d0e17b
zam <ahmed195138@feng.bu.edu.eg>
2024-03-25 14:39:07 +0200

    added logo

--- apps/frontend/src/pages/Editor/editorHeader.tsx
+++ apps/frontend/src/pages/Editor/editorHeader.tsx
@@ -1,15 +1,20 @@
 import { buttonVariants } from '@/components/ui/button';
+import { Logo } from '@/components/ui/logo';
 import { editorStore } from '@/lib/Editor/Models';
 import { cn } from '@/lib/cn';
 import { observer } from 'mobx-react-lite';
 import { Link, useParams } from 'react-router-dom';
-
 export const EditorHeader = observer(function EditorHeader() {
   const { workspaceId, appId } = useParams();
   const appName = editorStore.name;
   const currentPageId = editorStore.currentPageId;
   return (
-    <div className="flex w-full items-center gap-5 border-b px-6 py-[4px]">
+    <div className="flex w-full items-center gap-5 border-b px-3 py-[4px]">
+      <div className="h-full w-10">
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
       <p>{appName}</p>
       <Link
         className={cn(
