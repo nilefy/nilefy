5ede1f6e4dd944fd090d0b5d6f611b19474fb176
nagy nabil <nagynabil651@gmail.com>
2024-04-13 22:13:10 +0200

    remove soft delete from workspaces

--- packages/database/src/schema/schema.ts
+++ packages/database/src/schema/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from 'drizzle-orm';
+import { sql } from "drizzle-orm";
 import {
   integer,
   pgEnum,
@@ -9,42 +9,42 @@ import {
   varchar,
   unique,
   primaryKey,
-} from 'drizzle-orm/pg-core';
-import {permissionsTypes} from "@webloom/permissions";
+} from "drizzle-orm/pg-core";
+import { permissionsTypes } from "@webloom/permissions";
 
 /**
  * spread them to easy create createdAt and updatedAt fields
  */
 export const timeStamps = {
-  createdAt: timestamp('created_at')
+  createdAt: timestamp("created_at")
     .notNull()
     .default(sql`now()`),
-  updatedAt: timestamp('updated_at'),
+  updatedAt: timestamp("updated_at"),
 };
 
 /**
  *  spread to create `created_by_id` `updated_by_id` `deleted_by_id`
  */
 export const whoToBlame = {
-  createdById: integer('created_by_id')
+  createdById: integer("created_by_id")
     .references(() => users.id)
     .notNull(),
-  updatedById: integer('updated_by_id').references(() => users.id),
-  deletedById: integer('deleted_by_id').references(() => users.id),
+  updatedById: integer("updated_by_id").references(() => users.id),
+  deletedById: integer("deleted_by_id").references(() => users.id),
 };
 
 export const softDelete = {
-  deletedAt: timestamp('deleted_at'),
+  deletedAt: timestamp("deleted_at"),
 };
 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  username: varchar('username', { length: 256 }).notNull(),
-  email: varchar('email', { length: 256 }).unique().notNull(),
-  emailVerified: timestamp('emailVerified', { mode: 'date' }),
-  password: varchar('password', { length: 256 }),
-  avatar: text('avatar'),
-  conformationToken: varchar('conformation_token', {
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  username: varchar("username", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).unique().notNull(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  password: varchar("password", { length: 256 }),
+  avatar: text("avatar"),
+  conformationToken: varchar("conformation_token", {
     length: 256,
   }),
   ...timeStamps,
@@ -52,19 +52,19 @@ export const users = pgTable('users', {
 });
 
 export const accounts = pgTable(
-  'accounts',
+  "accounts",
   {
-    userId: integer('userId')
+    userId: integer("userId")
       .notNull()
-      .references(() => users.id, { onDelete: 'cascade' }),
-    provider: text('provider').notNull(),
-    providerAccountId: text('providerAccountId').notNull(),
+      .references(() => users.id, { onDelete: "cascade" }),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
   },
   (account) => ({
     compoundKey: primaryKey({
       columns: [account.provider, account.providerAccountId],
     }),
-  }),
+  })
 );
 
 /**
@@ -100,15 +100,15 @@ export const accounts = pgTable(
 // );
 
 export const pgPermissionsEnum = pgEnum(
-  'permissions_enum',
-  permissionsTypes.options,
+  "permissions_enum",
+  permissionsTypes.options
 );
 
-export const permissions = pgTable('permissions', {
-  id: serial('id').primaryKey(),
-  name: pgPermissionsEnum('name').unique().notNull(),
-  description: varchar('description', { length: 255 }),
-  createdAt: timestamp('created_at')
+export const permissions = pgTable("permissions", {
+  id: serial("id").primaryKey(),
+  name: pgPermissionsEnum("name").unique().notNull(),
+  description: varchar("description", { length: 255 }),
+  createdAt: timestamp("created_at")
     .notNull()
     .default(sql`now()`),
 });
@@ -121,15 +121,15 @@ export const permissions = pgTable('permissions', {
  * group could have more than one role, role could be in more than one group => many to many relation between roles and groups
  */
 export const roles = pgTable(
-  'roles',
+  "roles",
   {
-    id: serial('id').primaryKey(),
-    name: varchar('name', { length: 256 }).notNull(),
-    description: varchar('description', { length: 255 }),
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    description: varchar("description", { length: 255 }),
     /**
      * workspace this role belongs to
      */
-    workspaceId: integer('workspace_id')
+    workspaceId: integer("workspace_id")
       .references(() => workspaces.id)
       .notNull(),
     ...timeStamps,
@@ -139,37 +139,37 @@ export const roles = pgTable(
   (t) => ({
     // role name must be unique by workspace
     roleNameUnique: unique().on(t.workspaceId, t.name),
-  }),
+  })
 );
 
 export const permissionsToRoles = pgTable(
-  'permissions_to_roles',
+  "permissions_to_roles",
   {
-    permissionId: integer('permission_id')
+    permissionId: integer("permission_id")
       .notNull()
       .references(() => permissions.id),
-    roleId: integer('role_id')
+    roleId: integer("role_id")
       .notNull()
       .references(() => roles.id),
   },
   (t) => ({
     pk: primaryKey({ columns: [t.roleId, t.permissionId] }),
-  }),
+  })
 );
 
 export const usersToRoles = pgTable(
-  'users_to_roles',
+  "users_to_roles",
   {
-    userId: integer('user_id')
+    userId: integer("user_id")
       .notNull()
       .references(() => users.id),
-    roleId: integer('role_id')
+    roleId: integer("role_id")
       .notNull()
       .references(() => roles.id),
   },
   (t) => ({
     pk: primaryKey({ columns: [t.roleId, t.userId] }),
-  }),
+  })
 );
 
 // export const rolesToGroups = pgTable(
@@ -187,90 +187,89 @@ export const usersToRoles = pgTable(
 //   }),
 // );
 
-export const workspaces = pgTable('workspaces', {
-  id: serial('id').primaryKey(),
+export const workspaces = pgTable("workspaces", {
+  id: serial("id").primaryKey(),
   /**
    * implicitly means the admin
    */
-  createdById: integer('created_by_id')
+  createdById: integer("created_by_id")
     .references(() => users.id)
     .notNull(),
-  name: varchar('name', { length: 256 }).notNull(),
-  imageUrl: text('imageUrl'),
+  name: varchar("name", { length: 256 }).notNull(),
+  imageUrl: text("imageUrl"),
   ...timeStamps,
-  ...softDelete,
-  updatedById: integer('updated_by_id').references(() => users.id),
-  deletedById: integer('deleted_by_id').references(() => users.id),
+  updatedById: integer("updated_by_id").references(() => users.id),
+  deletedById: integer("deleted_by_id").references(() => users.id),
 });
 
 export const usersToWorkspaces = pgTable(
-  'users_to_workspaces',
+  "users_to_workspaces",
   {
-    userId: integer('user_id')
+    userId: integer("user_id")
       .notNull()
       .references(() => users.id),
-    workspaceId: integer('workspace_id')
+    workspaceId: integer("workspace_id")
       .notNull()
       .references(() => workspaces.id),
   },
   (t) => ({
     pk: primaryKey({ columns: [t.userId, t.workspaceId] }),
-  }),
+  })
 );
 
-export const apps = pgTable('apps', {
-  id: serial('id').primaryKey(),
-  createdById: integer('created_by_id')
+export const apps = pgTable("apps", {
+  id: serial("id").primaryKey(),
+  createdById: integer("created_by_id")
     .references(() => users.id)
     .notNull(),
-  name: varchar('name', { length: 100 }).notNull(),
-  description: varchar('description', { length: 255 }),
+  name: varchar("name", { length: 100 }).notNull(),
+  description: varchar("description", { length: 255 }),
   /**
    * workspace this app belongs to
    */
-  workspaceId: integer('workspace_id')
+  workspaceId: integer("workspace_id")
     .references(() => workspaces.id)
     .notNull(),
   ...timeStamps,
   ...softDelete,
-  updatedById: integer('updated_by_id').references(() => users.id),
-  deletedById: integer('deleted_by_id').references(() => users.id),
+  updatedById: integer("updated_by_id").references(() => users.id),
+  deletedById: integer("deleted_by_id").references(() => users.id),
 });
 
-export const webloomTables = pgTable('tables', {
-  id: serial('id').primaryKey(),
-  name: text('name').unique().notNull(),
+export const webloomTables = pgTable("tables", {
+  id: serial("id").primaryKey(),
+  name: text("name").unique().notNull(),
   ...timeStamps,
-  createdById: integer('created_by_id')
+  createdById: integer("created_by_id")
     .references(() => users.id)
     .notNull(),
   /**
    * workspace id this table belongs to
    */
-  workspaceId: integer('workspace_id')
+  workspaceId: integer("workspace_id")
     .references(() => workspaces.id)
     .notNull(),
 });
 
-export const pgColumnTypsEnum = pgEnum('pg_columns_enum', [
-  'varchar',
-  'int',
-  'bigint',
-  'serial',
-  'boolean',
+export const pgColumnTypsEnum = pgEnum("pg_columns_enum", [
+  "varchar",
+  "int",
+  "bigint",
+  "serial",
+  "boolean",
 ]);
 
 export const webloomColumns = pgTable(
-  'columns',
+  "columns",
   {
-    id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-    type: pgColumnTypsEnum('type').notNull(),
-    tableId: integer('table_id')
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    type: pgColumnTypsEnum("type").notNull(),
+    tableId: integer("table_id")
       .notNull()
-      .references(() => webloomTables.id, { onDelete: 'cascade' }),
+      .references(() => webloomTables.id, { onDelete: "cascade" }),
   },
   (t) => ({
     name: unique().on(t.tableId, t.name),
-  }),
+  })
 );
