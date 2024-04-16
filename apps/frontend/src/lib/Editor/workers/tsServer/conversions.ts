92bda9954507a64f671aab6573de28abde1fa256
zam <ahmed195138@feng.bu.edu.eg>
2024-04-16 16:44:17 +0200

    return unknown type for data jsonToTs cannot parse

--- apps/frontend/src/lib/Editor/workers/tsServer/conversions.ts
+++ apps/frontend/src/lib/Editor/workers/tsServer/conversions.ts
@@ -2,10 +2,14 @@ import JsonToTs from 'nilefy-json-to-ts';
 const SAMPLE_SIZE = 30;
 
 export function jsonToTs(typeName: string, json: any) {
-  if (!json) return 'undefined';
-  if (Array.isArray(json)) {
-    json = json.slice(0, SAMPLE_SIZE);
+  if (!json) return 'unknown';
+  try {
+    if (Array.isArray(json)) {
+      json = json.slice(0, SAMPLE_SIZE);
+    }
+    const res = JsonToTs(json, { named: false, dedupe: false })[0];
+    return res;
+  } catch (_) {
+    return 'unknown';
   }
-  const res = JsonToTs(json, { named: false, dedupe: false })[0];
-  return res;
 }
