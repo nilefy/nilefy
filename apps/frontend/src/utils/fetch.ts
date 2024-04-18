d25b37af2a0cabb0edf0efe6cdba65f94e62618f
nagy nabil <nagynabil651@gmail.com>
2024-04-18 14:07:12 +0200

    serve front from the backend in production

--- apps/frontend/src/utils/fetch.ts
+++ apps/frontend/src/utils/fetch.ts
@@ -1,13 +1,22 @@
 import { getToken } from '@/lib/token.localstorage';
 
-const baseUrl = 'http://localhost:3000/';
 /**
  * takes the url as relative url then it concat it to the baseUrl of the API
  * fetch extended is just a wrapper around fetch that throws an error if the response is not ok
  */
 export async function fetchX(...args: Parameters<typeof fetch>) {
   const [url, init] = args;
-  const endpoint = new URL(url as string | URL, baseUrl);
+  // if url starts with / add "api", if the url starts with http add nothing, if the url starts with https add nothing, if the url starts with anything else add "api\"
+  let endpoint = url;
+  if (typeof url === 'string' && url.startsWith('/')) {
+    endpoint = `api${url}`;
+  } else {
+    try {
+      new URL(url as string);
+    } catch {
+      endpoint = `api/${url}`;
+    }
+  }
   const res = await fetch(endpoint, {
     ...init,
     headers: {
