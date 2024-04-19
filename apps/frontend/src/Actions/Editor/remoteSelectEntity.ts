d02ef8d67e1b79bf5d00f67341e3616075a17c00
zam <ahmed195138@feng.bu.edu.eg>
2024-04-19 02:41:51 +0200

    fix: remote select scrollIntoView

--- apps/frontend/src/Actions/Editor/remoteSelectEntity.ts
+++ apps/frontend/src/Actions/Editor/remoteSelectEntity.ts
@@ -4,6 +4,7 @@ import { commandManager } from '../CommandManager';
 import { WidgetSelection } from './selection';
 import { editorStore } from '@/lib/Editor/Models';
 import { WebloomQuery } from '@/lib/Editor/Models/query';
+import scrollIntoView from 'scroll-into-view-if-needed';
 
 export class RemoteSelectEntity implements Command {
   constructor(private id: string) {}
@@ -11,7 +12,12 @@ export class RemoteSelectEntity implements Command {
     const entity = editorStore.getEntityById(this.id);
     if (entity instanceof WebloomWidget) {
       commandManager.executeCommand(WidgetSelection.remoteSelect(this.id));
-      entity.dom?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      scrollIntoView(entity.dom!, {
+        scrollMode: 'if-needed',
+        skipOverflowHiddenElements: true,
+        behavior: 'smooth',
+        block: 'center',
+      });
     }
     if (entity instanceof WebloomQuery) {
       editorStore.setSelectedQueryId(this.id);
