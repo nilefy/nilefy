2218c896e920bff439aa2ebf6745d049f354c0d3
Mohamedhadhoud1 <muhammed195837@feng.bu.edu.eg>
2024-05-28 11:17:36 +0300

    You can now style your html

--- apps/frontend/src/pages/Editor/Components/WebloomWidgets/Text/helper.tsx
+++ apps/frontend/src/pages/Editor/Components/WebloomWidgets/Text/helper.tsx
@@ -1,10 +1,10 @@
 import rehypeParse from 'rehype-parse';
-import rehypeSanitize from 'rehype-sanitize';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import rehypeStringify from 'rehype-stringify';
 import Markdown from 'markdown-to-jsx';
 import { unified } from 'unified';
 import { useState, useEffect } from 'react';
-
+import rehypeHighlight from 'rehype-highlight';
 export const useDataFetcher = (text: string) => {
   const [content, setContent] = useState<React.ReactElement | null>(null);
 
@@ -13,10 +13,24 @@ export const useDataFetcher = (text: string) => {
       try {
         const processedFile = await unified()
           .use(rehypeParse, { fragment: true })
-          .use(rehypeSanitize)
+          .use(rehypeSanitize, {
+            ...defaultSchema,
+            attributes: {
+              ...defaultSchema.attributes,
+              h1: ['style'],
+              h2: ['style'],
+              h3: ['style'],
+              h4: ['style'],
+              h5: ['style'],
+              h6: ['style'],
+              p: ['style'],
+              span: ['style'],
+              div: ['style'],
+            },
+          })
           .use(rehypeStringify)
           .process(text);
-
+        console.log(String(processedFile.value), 'kk');
         setContent(<Markdown>{String(processedFile)}</Markdown>);
       } catch (error) {
         console.error('Error processing file:', error);
